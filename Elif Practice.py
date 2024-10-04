b385f4122788c60fbb181dd5cde2bb055ddd9895
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-10-04 09:07:05 -0600

    Create Elif Practice.py

---
+++ Elif Practice.py
@@ -0,0 +1,56 @@
+# ELIF-STATEMENTS
+print("Number 1:")
+# Q1: Write an elif statement:
+# If temperature is greater than 25 print "its hot outside" 
+# elif temperature is between 15 and 25 print "its nice outside"
+# else print "it is cold outside"
+
+# Do not touch this code: 
+temperature = 21
+
+# Write your code here:
+
+print()
+print("Number 2:")
+# Q2: The ticket price for a carnival for someone whose age is greater than or equal to 65 is 4.75. Kids 12 and under pay 4.25. Everyone else pays 7.50. Write an if-elif statement to print the correct price based on age.
+
+# Do not touch this code: 
+age = 14
+
+# Write your code here:
+
+print()
+print("Number 3:")
+# Q3: Using input, ask for the user’s mark (out of 100). 
+# Then display their grade according to the marks entered as below:
+# Marks        Grade
+# 91-100         A+
+# 81-90          A-
+# 71-80          B+
+# 61-70          B-
+# 51-60          C+
+# any mark less than 50 = Fail
+
+print()
+print("Number 4:")
+# Q4: Ask for an integer through input. If the integer is even, print “This number is even”. Else print “This number is odd”. HINT: Use modulo! Think about what it means for an integer to be even.
+
+print()
+print("Number 5:")
+# Q5: Write a program that asks the user to enter a number. 
+# if the number is a multiple of three print “Fizz” instead of the number 
+# if the number is a multiple of five print “Buzz”. 
+# If the number is a multiple of both three and five print “FizzBuzz”.
+# else print the number
+
+print()
+print("Number 6:")
+#Q6: Write a program that asks the user to enter a number, and then prints "Positive" if the number is positive, "Negative" if the number is negative, or "Zero" if the number is zero.
+
+print()
+print("Number 7:")
+# Q7: Write a program that asks the user to enter a letter, and then prints out "The letter is a vowel" if the letter is a, e, i, o, or u (both upper and lower case), or "The letter is a consonant" if it is not a vowel (Assume that the user will always enter the letters LOWERCASE).
+
+print()
+print("Number 8:")
+# Q8: Write a program that asks the user to enter a number, and then prints out whether the number is a multiple of 3 or not.
